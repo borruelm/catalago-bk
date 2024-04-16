61b27cc92c7990ab5f0bba5e7a5a994c5d6da212
Maos95 <144496077+Maos95@users.noreply.github.com>
2024-04-15 21:36:47 -0600

    primer

---
+++ contruction.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Página en Construcción</title>
+<link rel="stylesheet" href="css/estilos.css" type="text/css">
+<style>
+    h1 {
+        font-size: 2em;
+        color: #333;
+    }
+    p {
+        font-size: 1.2em;
+        color: #666;
+    }
+</style>
+<style>
+body{
+margin: 0;
+padding: 0%;
+background-image: url(images/const.jpg);
+background-size: cover;
+background-position: center;
+}
+
+</style>
+</head>
+<body>
+    <div>
+        <h1>Página en Construcción</h1>
+        <p>Estamos trabajando para mejorar nuestra página. ¡Vuelve pronto!</p>
+    </div>
+</body>
+</html>
